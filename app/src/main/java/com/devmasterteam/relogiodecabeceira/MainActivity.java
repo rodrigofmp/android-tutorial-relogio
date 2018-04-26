c8a97664cf44532d843f20ae2dd3f0dedda60338
Rodrigo Petters <rodrigo.petters@inovan.net>
2018-04-25 21:33:58 -0300

    View Holder

--- app/src/main/java/com/devmasterteam/relogiodecabeceira/MainActivity.java
+++ app/src/main/java/com/devmasterteam/relogiodecabeceira/MainActivity.java
@@ -2,13 +2,27 @@ package com.devmasterteam.relogiodecabeceira;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewHolder mViewHolder = new ViewHolder();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+
+        this.mViewHolder.mTextHourMinute = this.findViewById(R.id.text_hour_minute);
+        this.mViewHolder.mTextSeconds = this.findViewById(R.id.text_seconds);
+        this.mViewHolder.mCheckBattery = this.findViewById(R.id.check_battery);
+    }
+
+    private static class ViewHolder {
+        TextView mTextHourMinute;
+        TextView mTextSeconds;
+        CheckBox mCheckBattery;
     }
 }
